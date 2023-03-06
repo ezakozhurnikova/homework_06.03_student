24ed86cacf643489b437bd8635c9adba46a24cb2
ezakozhurnikova <ekaterina.o.zak@gmail.com>
2023-03-06 17:44:12 +0100

    second commit

---
+++ src/homework_Student/Student.java
@@ -0,0 +1,13 @@
+package homework_Student;
+
+public class Student {
+    String name;
+    int number;
+    Student(){};
+
+    Student(String name,int number) {
+        this.name = name;
+        this.number = number;
+
+    }
+}
